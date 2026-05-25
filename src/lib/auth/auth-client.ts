96c2c07c1cfff4d9c9d43b063854f0bf7cb206cf
robert thornton <robert.thornton92@gmail.com>
2026-05-25 13:03:19 +0300

    feat: created react auth client for client side authentication actions

---
+++ src/lib/auth/auth-client.ts
@@ -0,0 +1,5 @@
+import { createAuthClient } from "better-auth/react";
+
+//  authClient is an adapter/utility for react components
+//  allowing them to interact with better-auth.
+export const authClient = createAuthClient();
